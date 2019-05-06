3c80da9acc76be6226e85419be2c966ed5bc9a86
anton <anton@codesigned.co.uk>
2019-05-07 00:15:42 +0100

    Start on view configs

--- public/app.js
+++ public/app.js
@@ -1,45 +1,59 @@
-document.addEventListener("DOMContentLoaded", event => {
+// helper
+let diff = (a1, a2) => a1.filter(x => !a2.includes(x))
+
+function addTextToRow(row, text, cellType) {
+  let cell = cellType ? document.createElement(cellType) : document.createElement('td')
+  cell.innerText = text
+  row.appendChild(cell)
+}
+
+function displayCollection(collectionName, viewConfig) {
   const db = firebase.firestore()
+  let table = document.querySelector('#docs')
+  table.innerHTML = ''
 
-  db.collection('events').get().then(function(querySnapshot) {
-    let table = document.querySelector('#docs')
-    let content = `
-      <tr>
-        <th>name</th>
-        <th>description</th>
-        <th>attendees</th>
-      </tr>
-    `
-    table.innerHTML = content
+  db.collection(collectionName).get().then(function(querySnapshot) {
+    let fields = viewConfig.fields
+    let hrow = document.createElement('tr')
+    table.appendChild(hrow)
+    fields.map( field => addTextToRow(hrow, field, 'th'))
 
-    querySnapshot.forEach(function(doc) {
+    let specialFields = viewConfig.specialFields ? Object.keys(viewConfig.specialFields) : []
+    querySnapshot.forEach(function(rawDoc) {
+      let doc = rawDoc.data()
       let row = document.createElement('tr')
       table.appendChild(row)
-
-      let nameCell = document.createElement('td')
-      nameCell.innerText = doc.data().name
-      row.appendChild(nameCell)
-
-      let descrCell = document.createElement('td')
-      descrCell.innerText = doc.data().description
-      row.appendChild(descrCell)
-
-      let attendeesCell = document.createElement('td')
-      row.appendChild(attendeesCell)
-      insertNameList(attendeesCell, db, doc.data().attendees)
-    });
-  });
-})
-
-function insertNameList(element, db, attendees) {
-  attendees.forEach( ref => {
-    let personDoc = db.collection('people').doc(ref.id)
-    personDoc.get().then(person => {
-      let personLink = document.createElement('a')
-      personLink.setAttribute('href', '#')
-      personLink.addEventListener('click', () => console.log('show '+person.data().name))
-      personLink.innerText = person.data().name
-      element.appendChild(personLink)
+      fields.map( field => {
+        if (specialFields.includes(field)) {
+          let cell = document.createElement('td')
+          row.appendChild(cell)
+          viewConfig.specialFields[field](doc, cell)
+        } else addTextToRow(row, doc[field])
+      })
     })
   })
 }
+
+
+document.addEventListener("DOMContentLoaded", event => {
+  let eventsView = {
+    fields: ['name', 'description', 'attendees'],
+    specialFields: {
+      attendees: (eventDoc, attendeesCell) => {
+        //put data from doc into cell
+        let seePeopleLink = document.createElement('a')
+        seePeopleLink.innerText = 'See attendees'
+        seePeopleLink.setAttribute('href', '#')
+        seePeopleLink.addEventListener('click', () => {
+          console.log('show attendees for '+eventDoc.name)
+          displayCollection('people', peopleView)
+        })
+        attendeesCell.appendChild(seePeopleLink)
+        let peopleView = {
+          fields: ['name']
+        }
+      }
+    }
+  }
+  displayCollection('events', eventsView)
+})
