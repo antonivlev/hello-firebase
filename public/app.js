8f9346e604b3d1513cd1a214a832110ffb98516b
anton <anton@codesigned.co.uk>
2019-05-12 11:39:05 +0100

    Finish view configs, sort out queries wiuth refs
    Db had wrong data type in it

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-// helper
+// helper - array difference
 let diff = (a1, a2) => a1.filter(x => !a2.includes(x))
 
 function addTextToRow(row, text, cellType) {
@@ -7,12 +7,37 @@ function addTextToRow(row, text, cellType) {
   row.appendChild(cell)
 }
 
+
+// let eventsView = {
+//   fields: ['name', 'description', 'attendees'],
+//   linkFields: {
+//     attendees: (doc) => {
+//       let peopleView = {
+//         fields: ['name', 'attended'],
+//         whereArgs: ['attended', 'array-contains', doc.eventRef],
+//         linkFields: {
+//           attended: displayCollection('events', attendedConfig)
+//         }
+//       }
+//       displayCollection('people', peopleView)
+//     }
+//   }
+// }
+//
+// displayCollection('events', eventsView)
+
+
 function displayCollection(collectionName, viewConfig) {
+  console.log('Displaying ', collectionName, ' where: ', viewConfig.whereArgs)
   const db = firebase.firestore()
   let table = document.querySelector('#docs')
   table.innerHTML = ''
 
-  db.collection(collectionName).get().then(function(querySnapshot) {
+  let collectionPromise = viewConfig.whereArgs ?
+    db.collection(collectionName).where(...viewConfig.whereArgs).get() :
+    db.collection(collectionName).get()
+
+  collectionPromise.then(function(querySnapshot) {
     let fields = viewConfig.fields
     let hrow = document.createElement('tr')
     table.appendChild(hrow)
@@ -27,31 +52,32 @@ function displayCollection(collectionName, viewConfig) {
         if (specialFields.includes(field)) {
           let cell = document.createElement('td')
           row.appendChild(cell)
-          viewConfig.specialFields[field](doc, cell)
+          viewConfig.specialFields[field](rawDoc, cell)
         } else addTextToRow(row, doc[field])
       })
     })
   })
 }
 
-
 document.addEventListener("DOMContentLoaded", event => {
+  db = firebase.firestore()
+
   let eventsView = {
     fields: ['name', 'description', 'attendees'],
     specialFields: {
-      attendees: (eventDoc, attendeesCell) => {
+      attendees: (eventRawDoc, attendeesCell) => {
         //put data from doc into cell
         let seePeopleLink = document.createElement('a')
         seePeopleLink.innerText = 'See attendees'
         seePeopleLink.setAttribute('href', '#')
         seePeopleLink.addEventListener('click', () => {
-          console.log('show attendees for '+eventDoc.name)
+          let peopleView = {
+            whereArgs: ['attended', 'array-contains', eventRawDoc.ref],
+            fields: ['name']
+          }
           displayCollection('people', peopleView)
         })
         attendeesCell.appendChild(seePeopleLink)
-        let peopleView = {
-          fields: ['name']
-        }
       }
     }
   }
