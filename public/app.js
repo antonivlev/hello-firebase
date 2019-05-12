750b991a83905d1a7ca23e6fc7a66cd481ffad19
anton <anton@codesigned.co.uk>
2019-05-12 14:02:17 +0100

    Figuring out basic force

--- public/app.js
+++ public/app.js
@@ -1,85 +1,54 @@
-// helper - array difference
-let diff = (a1, a2) => a1.filter(x => !a2.includes(x))
-
-function addTextToRow(row, text, cellType) {
-  let cell = cellType ? document.createElement(cellType) : document.createElement('td')
-  cell.innerText = text
-  row.appendChild(cell)
-}
-
-
-// let eventsView = {
-//   fields: ['name', 'description', 'attendees'],
-//   linkFields: {
-//     attendees: (doc) => {
-//       let peopleView = {
-//         fields: ['name', 'attended'],
-//         whereArgs: ['attended', 'array-contains', doc.eventRef],
-//         linkFields: {
-//           attended: displayCollection('events', attendedConfig)
-//         }
-//       }
-//       displayCollection('people', peopleView)
-//     }
-//   }
-// }
-//
-// displayCollection('events', eventsView)
-
-
-function displayCollection(collectionName, viewConfig) {
-  console.log('Displaying ', collectionName, ' where: ', viewConfig.whereArgs)
-  const db = firebase.firestore()
-  let table = document.querySelector('#docs')
-  table.innerHTML = ''
-
-  let collectionPromise = viewConfig.whereArgs ?
-    db.collection(collectionName).where(...viewConfig.whereArgs).get() :
-    db.collection(collectionName).get()
-
-  collectionPromise.then(function(querySnapshot) {
-    let fields = viewConfig.fields
-    let hrow = document.createElement('tr')
-    table.appendChild(hrow)
-    fields.map( field => addTextToRow(hrow, field, 'th'))
-
-    let specialFields = viewConfig.specialFields ? Object.keys(viewConfig.specialFields) : []
-    querySnapshot.forEach(function(rawDoc) {
-      let doc = rawDoc.data()
-      let row = document.createElement('tr')
-      table.appendChild(row)
-      fields.map( field => {
-        if (specialFields.includes(field)) {
-          let cell = document.createElement('td')
-          row.appendChild(cell)
-          viewConfig.specialFields[field](rawDoc, cell)
-        } else addTextToRow(row, doc[field])
-      })
-    })
-  })
-}
-
-document.addEventListener("DOMContentLoaded", event => {
-  db = firebase.firestore()
-
-  let eventsView = {
-    fields: ['name', 'description', 'attendees'],
-    specialFields: {
-      attendees: (eventRawDoc, attendeesCell) => {
-        //put data from doc into cell
-        let seePeopleLink = document.createElement('a')
-        seePeopleLink.innerText = 'See attendees'
-        seePeopleLink.setAttribute('href', '#')
-        seePeopleLink.addEventListener('click', () => {
-          let peopleView = {
-            whereArgs: ['attended', 'array-contains', eventRawDoc.ref],
-            fields: ['name']
-          }
-          displayCollection('people', peopleView)
-        })
-        attendeesCell.appendChild(seePeopleLink)
-      }
-    }
+var events = [
+  {
+    id: 'lunch',
+    attendees: ['dave', 'sam', 'bob']
+  },
+  {
+    id: 'dinner',
+    attendees: ['josh', 'bob']
   }
-  displayCollection('events', eventsView)
-})
+]
+
+var people = [
+  {
+    id: 'dave',
+    attended: ['lunch']
+  },
+  {
+    id: 'sam',
+    attended: ['lunch']
+  },
+  {
+    id: 'bob',
+    attended: ['lunch', 'dinner']
+  },
+  {
+    id: 'josh',
+    attended: ['dinner']
+  },
+]
+
+var width = 300, height = 300
+var svg = d3.select('body').append('svg')
+  .attr('width', width)
+  .attr('height', height);
+
+
+var simulation = d3.forceSimulation(people)
+  .force('charge', d3.forceManyBody())
+  .force('center', d3.forceCenter(width / 2, height / 2))
+  // ^ added x, y, vx, vy, to each obj in people. These update according to forces. 
+  .on('tick', () => {})
+  .on('end', draw);
+  // ^ say what to do in various parts of the sim
+
+function draw() {
+  console.log('drawing');
+  svg.selectAll('circle')
+    .data(people)
+    .enter().append('circle')
+      .attr('cx', d => d.x)
+      .attr('cy', d => d.y)
+      .attr('id', d => d.id)
+      .attr('r', 3);
+}
