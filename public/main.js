fc71efef86573ddd7ce5adb3e7270290a69c4c36
anton <anton.ivlev97@gmail.com>
2019-05-18 23:02:26 +0100

    Add zooming in

--- public/main.js
+++ public/main.js
@@ -2,16 +2,34 @@
 var width = 300, height = 300
 var svg = d3.select('body').append('svg')
   .attr('width', width)
-  .attr('height', height);
+  .attr('height', height)
+  .call(d3.zoom()
+    .on("zoom", zoomed));
+
+var g = svg.append('g');
+
+function zoomed() {
+  g.attr("transform", d3.event.transform);
+}
 
 var info = d3.select('body').append('h3')
   .attr('id', 'id-info');
 
+
 // global data stuff
 var nodes = [];
 var links = [];
-var sim = setUpSim(svg, nodes, links);
+var sim = setUpSim(g, nodes, links);
 
+var button = d3.select('body').append('button')
+  .on('click', () => {
+    nodes.push( {id: 'new-boi'} );
+    links.push( {source: 'new-boi', target: 'emma'} );
+    sim.nodes(nodes);
+    sim.alpha(0.5);
+    sim.restart();
+  })
+  .text('add node');
 
 document.addEventListener("DOMContentLoaded", event => {
   const db = firebase.firestore()
@@ -20,21 +38,23 @@ document.addEventListener("DOMContentLoaded", event => {
     .then(querySnapshot => {
       querySnapshot.forEach(eventDoc => {
         let docData = eventDoc.data();
+
         nodes.push( {id: eventDoc.id, ...docData, event: true} );
+        sim.nodes(nodes);
+        sim.alpha(1);
+        sim.restart();
+
         docData.attendees.forEach(
           ref => ref.get().then(
             personDoc => {
-              //console.log(doc.id);
               links.push( {source: eventDoc.id, target: personDoc.id} )
               nodes.push( {id: personDoc.id, ...personDoc.data()} );
               sim.nodes(nodes);
               sim.alpha(1);
+              sim.restart();
             }
           )
         );
       });
-    })
-})
-
-// var {fnodes, flinks} = getData();
-// setUpSim(svg, fnodes, flinks);
+    });
+});
--- public/simFuncs.js
+++ public/simFuncs.js
@@ -1,17 +1,8 @@
-function setUpSim(svg, nodes, links) {
+function setUpSim(g, nodes, links) {
   // update rule: data -> elements
   function ticked() {
-    // ENTER
-    svg.selectAll('line')
-      .data(links)
-      .join('line')
-        .attr('x1', d => d.source.x)
-        .attr('y1', d => d.source.y)
-        .attr('x2', d => d.target.x)
-        .attr('y2', d => d.target.y);
-
     // append circle for each data point
-    svg.selectAll('circle')
+    g.selectAll('circle')
       .data(nodes)
       .join('circle')
         .attr('id', d => d.id)
@@ -22,15 +13,23 @@ function setUpSim(svg, nodes, links) {
         .on('mouseover', (_, i, circles) => circles[i].classList.add('selected'))
         .on('mouseout', (_, i, circles) => circles[i].classList.remove('selected'))
         .on('click', d => d3.select('#id-info').text('name: '+d.name) );
+
+    g.selectAll('line')
+      .data(links)
+      .join('line')
+        .attr('x1', d => d.source.x)
+        .attr('y1', d => d.source.y)
+        .attr('x2', d => d.target.x)
+        .attr('y2', d => d.target.y);
   }
 
   var simulation = d3.forceSimulation(nodes)
     // ^ adds x, y, vx, vy, to each obj in people. These update according to forces below.
-    .force('charge', d3.forceManyBody())
+    .force('charge', d3.forceManyBody().strength(() => -10))
     .force('center', d3.forceCenter(width / 2, height / 2))
     // adds x, y to source and target in links
     .force('links', d3.forceLink().id(d => d.id).links(links).distance(70))
-    .on('tick', ticked);
+    .on('tick', ticked)
     // ^ say what to do on internal tick of the sim
 
   return simulation;
